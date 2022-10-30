a6af020c95588db071c6968921ca3fd8156c864c
Sofia <sofiacaldas1999@gmail.com>
2022-10-30 13:52:15 +0000

    Adicionei print de "Estou no git"

--- src/pt/ulusofona/deisi/lp2/contaBancaria/Main.java
+++ src/pt/ulusofona/deisi/lp2/contaBancaria/Main.java
@@ -3,6 +3,10 @@ package pt.ulusofona.deisi.lp2.contaBancaria;
 public class Main {
 
     public static void main(String[] args) {
-
+        ContaBancaria conta = new ContaBancaria(100);
+        conta.depositar(50);
+        conta.levanta(70);
+        System.out.println(conta.getSaldoComoString());
+        System.out.println("Estou no git!");
     }
 }
